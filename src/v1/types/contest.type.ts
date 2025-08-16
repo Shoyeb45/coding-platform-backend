eec1265adbbddcd7681e17e9f456eb50fcab2cdf
Shoyeb45 <shoyebff45@gmail.com>
2025-08-17 01:17:41 +0530

    Refactor contest type definitions to improve structure and clarity

--- src/v1/types/contest.type.ts
+++ src/v1/types/contest.type.ts
@@ -1,11 +1,32 @@
 import z from "zod";
 
-export const ZContestCreation = z.object({
-    name: z.string(),
-    description: z.string(),
-    startTime: z.iso.datetime(),
-    endTime: z.iso.datetime(),
-    createBy: z.string() 
+export const ZContestCreate = z.object({
+    title: z.string().min(1, "The contest name should contain at least 1 character."),
+    createdBy: z.string()
 });
 
-export type TContestCreation = z.infer<typeof ZContestCreation>
+export const ZContest = z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    startTime: z.iso.datetime().optional(),
+    batches: z.array(z.string()).optional(),
+    isOpen: z.boolean().default(false),
+    moderators: z.array(z.string()).optional(),
+    topics: z.array(z.string()).optional(),
+    endTime: z.iso.datetime().optional(),
+    languages: z.array(z.string()).optional(),
+});
+
+export const ZContestProblem = z.object({
+    problemId: z.string(),
+    points: z.number()
+});
+
+export const ZContestMod = z.object({
+    moderatorId: z.string() 
+});
+
+export type TContestMod = z.infer<typeof ZContestMod>;
+export type TContestProblem = z.infer<typeof ZContestProblem>;
+export type TContest = z.infer<typeof ZContest>
+export type TContestCreate = z.infer<typeof ZContestCreate>
