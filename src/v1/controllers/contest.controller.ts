32dd7a67a5f4562babe7cd1d2cfbdbf00f09d24b
Shoyeb45 <shoyebff45@gmail.com>
2025-08-19 19:31:14 +0530

    fix: include user context in contest service methods for moderator and problem management

--- src/v1/controllers/contest.controller.ts
+++ src/v1/controllers/contest.controller.ts
@@ -14,22 +14,23 @@ export class ContestController {
 
     static addModerator = async (req: Request, res: Response) => {
         const contestId = req.params.contestId;
-        const data = await ContestService.addModerator(contestId, req.body);
+        const data = await ContestService.addModerator(req.user, contestId, req.body);
         res.status(HTTP_STATUS.CREATED).json(
             new ApiResponse("Successfully added moderator the contest.", data)
         )
     } 
 
+    
     static deleteProblemFromContest = async (req: Request, res: Response) => {
         const contestId = req.params.contestId;
-        await ContestService.deleteProblemFromContest(contestId, req.body.problemId);
+        await ContestService.deleteProblemFromContest(req.user, contestId, req.body.problemId);
         res.status(HTTP_STATUS.OK).json(
             new ApiResponse("Successfully deleted problem from the contest.", {})
         );
     }
     static deleteModerator = async (req: Request, res: Response) => {
         const contestId = req.params.contestId;
-        await ContestService.deleteModerator(contestId, req.body);
+        await ContestService.deleteModerator(req.user, contestId, req.body);
         res.status(HTTP_STATUS.CREATED).json(
             new ApiResponse("Successfully deleted moderator from the contest.", {})
         )
@@ -46,7 +47,7 @@ export class ContestController {
         );
     }
     static createContest = async (req: Request, res: Response): Promise<void> => {
-        const contestDetail = await ContestService.createContest(req.body);
+        const contestDetail = await ContestService.createContest(req.user, req.body);
 
         res.status(HTTP_STATUS.CREATED).json(
             new ApiResponse("Contest created successfully", { contestDetail })
