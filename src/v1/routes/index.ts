db1dd3f0fcd30a814818cf69a27e591416ffd00b
Shoyeb45 <shoyebff45@gmail.com>
2025-08-19 08:52:25 +0530

    Add submission router to main routes

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -4,6 +4,7 @@ import problemRouter from "./problem.route";
 import tagRouter from "./tag.route";
 import testcaseRouter from "./testcase.route";
 import runRouter from "./run.route";
+import submissionRoter from "./submission.route";
 
 export const router = Router();
 
@@ -12,3 +13,4 @@ router.use("/problems", problemRouter);
 router.use("/tags", tagRouter);
 router.use("/testcases", testcaseRouter);
 router.use("/run", runRouter);
+router.use("/submissions", submissionRoter);
