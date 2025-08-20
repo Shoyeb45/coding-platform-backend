98ace4c53c03faec686dc7ecb1dee46b4147784c
Shoyeb45 <shoyebff45@gmail.com>
2025-08-20 17:46:36 +0530

    added comprehensive documentation in contest routes

--- src/v1/routes/contest.route.ts
+++ src/v1/routes/contest.route.ts
@@ -9,23 +9,264 @@ const router = Router();
 
 router.use(authenticateUser);
 
+/**
+ * POST /
+ * @description Create a contest
+ * @body : {
+ *     title: string
+ * }
+ * @returns 
+ * data: {
+ *      contestDetail: {
+            title: string;
+            description: string;
+            startTime: Date;
+            endTime: Date;
+            creator: {
+                id: string;
+                name: string;
+                email: string;
+                designation: string | null;
+            };
+        }
+    }      
+ * 
+ */
 router.route("/")
-    .post(validate(ZContestCreate), asyncHandler(ContestController.createContest))
+    .post(validate(ZContestCreate), asyncHandler(ContestController.createContest));
+
+/**
+ * GET /
+ * @description Get all contests of the teacher
+ * @returns 
+ * data: {
+ *   contests: {
+        batchContests: {
+            id: string;
+            name: string;
+        }[];
+        contestModerators: {
+            id: string;
+            name: string;
+            email: string;
+        }[];
+        tags: {
+            id: string;
+            name: string;
+        }[];
+        allowedLanguages: {
+            id: string;
+            name: string;
+        }[];
+        title: string;
+        description: string;
+        startTime: Date;
+        endTime: Date;
+        id: string;
+    }[]
+ * }
+ */
+router.route("/")
     .get(asyncHandler(ContestController.getContests));
 
+/**
+ * PATCH /:contestId
+ * @description To edit the contest
+ * @param contestId -> Id of the contest
+ * @body
+ {
+    isOpen: boolean;
+    title?: string | undefined;
+    description?: string | undefined;
+    startTime?: string | undefined;
+    batches?: string[] | undefined;
+    moderators?: string[] | undefined;
+    topics?: string[] | undefined;
+    endTime?: string | undefined;
+    languages?: string[] | undefined;
+ }
+ * @returns
+ * data: {
+ *  updatedContestData: {
+        batchContests: {
+            id: string;
+            name: string;
+        }[];
+        contestModerators: {
+            id: string;
+            name: string;
+            email: string;
+        }[];
+        tags: {
+            id: string;
+            name: string;
+        }[];
+        allowedLanguages: {
+            id: string;
+            name: string;
+        }[];
+        title: string;
+        description: string;
+        startTime: Date;
+        endTime: Date;
+        id: string;
+    }
+}
+ */
 router.route("/:contestId")
-    .patch(validate(ZContest), asyncHandler(ContestController.updateContest))
+    .patch(validate(ZContest), asyncHandler(ContestController.updateContest));
+
+/**
+ * GET /:contestId
+ * @description get the contest detail
+ * @param contestId -> Id of the contest
+ * @body
+ {
+    isOpen: boolean;
+    title?: string | undefined;
+    description?: string | undefined;
+    startTime?: string | undefined;
+    batches?: string[] | undefined;
+    moderators?: string[] | undefined;
+    topics?: string[] | undefined;
+    endTime?: string | undefined;
+    languages?: string[] | undefined;
+ }
+ * @returns
+ * data: {
+ *  contestDatails: {
+        batchContests: {
+            id: string;
+            name: string;
+        }[];
+        contestModerators: {
+            id: string;
+            name: string;
+            email: string;
+        }[];
+        tags: {
+            id: string;
+            name: string;
+        }[];
+        allowedLanguages: {
+            id: string;
+            name: string;
+        }[];
+        title: string;
+        description: string;
+        startTime: Date;
+        endTime: Date;
+        id: string;
+    }
+}
+ */
+router.route("/:contestId")
     .get(asyncHandler(ContestController.getContest));
 
 
+/**
+ * GET /problem/:contestId
+ * @description Get all the problems of particular contest
+ * @param contestId id of the contest
+ * @returns
+ * data: {
+ *      problems: {
+            id: string;
+            title: string;
+            difficulty: $Enums.Difficulty;
+            points: number;
+        }[]
+ * }
+ */
 router.route("/problem/:contestId")
-    .get(asyncHandler(ContestController.getAllProblems))
-    .post(validate(ZContestProblem), asyncHandler(ContestController.addProblemToContest))
-    .delete(validate(ZContestProblem.pick({ problemId: true })), asyncHandler(ContestController.deleteProblemFromContest))
+    .get(asyncHandler(ContestController.getAllProblems));
 
+/**
+ * POST /problem/:contestId
+ * @description add problem to the contest
+ * @param contestId: id of the contest 
+ * @body
+ * {
+ *    problemId: string
+ * }
+ * @returns
+ * data: {
+      problemDetail: {
+        points: number;
+        problemId: string;
+        title: string;
+    }
+ * }
+ */
+router.route("/problem/:contestId")
+    .post(validate(ZContestProblem), asyncHandler(ContestController.addProblemToContest));
+
+/**
+ * DELETE /problem/:contestId
+ * @description Delete the problem from the contest
+ * @param contestId id of the contest
+ * @body
+ * {
+ *    problemId: string
+ * }
+ * @returns 
+ * data: {}
+ */
+router.route("/problem/:contestId")
+    .delete(validate(ZContestProblem.pick({ problemId: true })), asyncHandler(ContestController.deleteProblemFromContest));
+
+
+/**
+ * POST /moderators/:contestId
+ * @description API to add the moderator in the contest
+ * @param contestId id of the contest 
+ * @body
+ * {
+ *    moderatorId: string
+ * }
+ * @returns
+ *  data: {
+        moderator: {
+            id: string;
+            name: string;
+            email: string;
+            designation: string | null;
+        };
+ *  }
+ */
 router.route("/moderators/:contestId")
-    .post(validate(ZContestMod), asyncHandler(ContestController.addModerator))  // for adding the mods
-    .get(asyncHandler(ContestController.getAllModerators))   // for getting all the mods
-    .delete(validate(ZContestMod), asyncHandler(ContestController.deleteModerator)) // for deleting the mods
+    .post(validate(ZContestMod), asyncHandler(ContestController.addModerator));
+
+
+/**
+ * GET /moderators/:contestId
+ * @description API to get all the moderators of the contest
+ * @param contestId id of the contest 
+ * @returns
+ *  data: {
+        moderators: {
+            id: string;
+            name: string;
+            email: string;
+            designation: string | null;
+        }[];
+ *  }
+ */
+router.route("/moderators/:contestId")
+    .get(asyncHandler(ContestController.getAllModerators));
+
+/**
+ * DELETE /moderators/:contestId
+ * @description API to get delete the moderator from the contest
+ * @param contestId id of the contest 
+ * @body
+ * {
+ *    moderatorId: string
+ * }
+ * @returns
+ *  data: { }
+ */
+router.route("/moderators/:contestId")
+    .delete(validate(ZContestMod), asyncHandler(ContestController.deleteModerator));
 
 export default router;
